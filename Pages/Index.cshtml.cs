89f3f5c7858dd06150076b1d3a707dca7d0d8cd4
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-15 20:58:57 -0500

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+using BabouExtensions;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
+
+namespace SiteRedirectTemplate.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _config;
+
+        public IndexModel(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult OnGet(int? id = null)
+        {
+            if (id != null)
+            {
+                var sqlConnectionString = _config.GetConnectionString("DefaultConnection");
+
+                using (var connection = new SqlConnection(sqlConnectionString))
+                {
+                    var shortenedUrl = connection.QueryFirst<AJT.Dtos.ShortenedUrlDto>("SELECT * FROM ShortenedUrls WHERE Token = @Token AND Domain = '@Domain'", new {Token = id, Domain = _config["Domain"]});
+
+                    if (shortenedUrl == null)
+                        return new RedirectResult($"https://api.ajt.io?Message=The token {id} no longer exists.", false);
+
+                    var click = new AJT.Dtos.ShortenedUrlClickDto()
+                    {
+                        ShortenedUrlId = shortenedUrl.Id,
+                        ClickDate = DateTime.Now,
+                        Referrer = HttpContext.Request.Headers[HeaderNames.Referer].ToString().Truncate(500, false)
+                    };
+
+                    connection.Execute("INSERT INTO ShortenedUrlClicks (ShortenedUrlId, ClickDate, Referrer) VALUES (@ShortenedUrlId, @ClickDate, @Referrer)", new {click.ShortenedUrlId, click.ClickDate, click.Referrer});
+
+                    return new RedirectResult(shortenedUrl.LongUrl, false);
+                }
+            }
+            else
+            {
+                return new RedirectResult($"https://api.ajt.io?Message=Create your own short short url by signing up here.", false);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace SiteRedirectTemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddJsonFile("appsettings.json", false, true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                    configApp.AddEnvironmentVariables();
+                    configApp.AddCommandLine(args);
+
+                    if (hostContext.HostingEnvironment.IsDevelopment())
+                    {
+                        configApp.AddUserSecrets<Program>();
+                    }
+                });
+    }
+}
