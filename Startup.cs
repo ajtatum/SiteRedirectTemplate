351b406073db46907637086d79454b7fe9e8469a
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-17 20:46:11 -0500

    Added Security Headers

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,12 @@
+using System;
+using Babou.AspNetCore.SecurityExtensions.ContentSecurityPolicy;
+using Babou.AspNetCore.SecurityExtensions.CustomHeaders;
+using Babou.AspNetCore.SecurityExtensions.ExpectCT;
+using Babou.AspNetCore.SecurityExtensions.FeaturePolicy;
+using Babou.AspNetCore.SecurityExtensions.FrameOptions;
+using Babou.AspNetCore.SecurityExtensions.ReferrerPolicy;
+using Babou.AspNetCore.SecurityExtensions.XContentTypeOptions;
+using Babou.AspNetCore.SecurityExtensions.XRobotsTag;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -20,6 +29,13 @@ namespace SiteRedirectTemplate
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +60,33 @@ namespace SiteRedirectTemplate
             fordwardedHeaderOptions.KnownProxies.Clear();
 
             app.UseForwardedHeaders(fordwardedHeaderOptions);
+
+            app.UseContentSecurityPolicy(new CspDirectiveList
+            {
+                DefaultSrc = CspDirective.Self.AddHttpsScheme(),
+                StyleSrc = StyleCspDirective.Self.AddUnsafeInline().AddHttpsScheme(),
+                ScriptSrc = ScriptCspDirective.Self.AddUnsafeInline().AddHttpsScheme(),
+                ImgSrc = CspDirective.Self.AddDataScheme().AddHttpsScheme(),
+                FontSrc = CspDirective.Self.AddHttpsScheme(),
+                ConnectSrc = CspDirective.Self.AddHttpsScheme()
+            });
+
+            app.UseFeaturePolicy(
+                new FeatureDirectiveList()
+                    .AddNone(PolicyFeature.Microphone)
+                    .AddNone(PolicyFeature.Camera)
+                    .AddSelf(PolicyFeature.FullScreen)
+            );
+
+            app.UseFrameOptions(FrameOptionsPolicy.Deny);
+
+            app.UseXContentTypeOptions(XContentTypeOptions.NoSniff);
+
+            app.UseReferrerPolicy(ReferrerPolicy.UnsafeUrl);
+
+            app.UseXRobotsTag(false, false);
+
+            app.UseExpectCT(true, new TimeSpan(7,0,0,0), new Uri("https://ajtio.report-uri.com/r/d/ct/enforce"));
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
